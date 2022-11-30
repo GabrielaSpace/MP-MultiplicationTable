bdc30365e057f714b35ce22d6e09207eea5abfa9
Gabriela Garcia Serrano <118684159+GabrielaSpace@users.noreply.github.com>
2022-12-01 00:11:21 +0100

    .

--- js/mulTable.js
+++ js/mulTable.js
@@ -5,8 +5,17 @@ Ejemplo --> Tabla del 3 --> [3,6,9,12,15,18,21,24,27,30]
 </p> */
 
 let numero =parseInt(prompt("Ingrese el número de la tabla que desea"));
+let tabla =[];
 
-for (let i = 0; i <= 10 ; i++) {
- 
+/* for (let i = 0; i <= 10 ; i++) {
     document.write(+numero+" x "+i+" = "+(numero*i)+"<br>");  
+
+} */
+function multiplicar(numero) {
+    
+
+for (let i = 1; i <= 10 ; i++) {
+    tabla.push(i*numero);
+}
+return tabla;
 }
